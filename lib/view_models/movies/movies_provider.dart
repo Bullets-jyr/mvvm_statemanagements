bb2d5aa40ca21702436303a7a9d2a15377bb3dd3
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2024-12-30 11:18:34 +0900

    feat: Create the movies provider

--- lib/view_models/movies/movies_provider.dart
+++ lib/view_models/movies/movies_provider.dart
@@ -1,36 +1,39 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-//
-// import '../../models/movies_model.dart';
-// import '../../repository/movies_repo.dart';
-// import '../../service/init_getit.dart';
-// import 'movies_state.dart';
-//
-// final moviesProvider = StateNotifierProvider<MoviesProvider, MoviesState>(
-//   (_) => MoviesProvider(),
-// );
-//
-// class MoviesProvider extends StateNotifier<MoviesState> {
-//   MoviesProvider() : super(MoviesState());
-//
-//   final MoviesRepository _moviesRepository = getIt<MoviesRepository>();
-//   Future<void> getMovies() async {
-//     state = state.copyWith(isLoading: true);
-//     try {
-//       if (state.genresList.isEmpty) {
-//         final genresList = await _moviesRepository.fetchGenres();
-//         state = state.copyWith(genresList: genresList);
-//       }
-//       List<MovieModel> movies =
-//           await _moviesRepository.fetchMovies(page: state.currentPage);
-//       state = state.copyWith(
-//         moviesList: [...state.moviesList, ...movies],
-//         currentPage: state.currentPage + 1,
-//         fetchMoviesError: '',
-//         isLoading: false,
-//       );
-//     } catch (e) {
-//       state = state.copyWith(fetchMoviesError: e.toString(), isLoading: false);
-//       rethrow;
-//     }
-//   }
-// }
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mvvm_statemanagements/models/movies_model.dart';
+import 'package:mvvm_statemanagements/repository/movies_repo.dart';
+import 'package:mvvm_statemanagements/service/init_getit.dart';
+import 'package:mvvm_statemanagements/view_models/movies/movies_state.dart';
+
+final moviesProvider = StateNotifierProvider<MoviesProvider, MoviesState>(
+  (_) => MoviesProvider(),
+);
+
+class MoviesProvider extends StateNotifier<MoviesState> {
+  MoviesProvider() : super(MoviesState());
+
+  final MoviesRepository _moviesRepository = getIt<MoviesRepository>();
+
+  Future<void> getMovies() async {
+    state = state.copyWith(isLoading: true);
+    try {
+      if (state.genresList.isEmpty) {
+        final genresList = await _moviesRepository.fetchGenres();
+        state = state.copyWith(genresList: genresList);
+      }
+      List<MovieModel> movies =
+          await _moviesRepository.fetchMovies(page: state.currentPage);
+      state = state.copyWith(
+        moviesList: [...state.moviesList, ...movies],
+        currentPage: state.currentPage + 1,
+        fetchMoviesError: '',
+        isLoading: false,
+      );
+    } catch (e) {
+      state = state.copyWith(
+        fetchMoviesError: e.toString(),
+        isLoading: false,
+      );
+      rethrow;
+    }
+  }
+}
